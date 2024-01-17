3fac101426c9d75ee77004fbe94ab162da187f8a
Alexandre <alexandresargitario@gmail.com>
2024-01-17 09:43:56 -0300

    a

--- lambda/index.js
+++ lambda/index.js
@@ -324,7 +324,13 @@ const HorarioCoordenadorIntentHandler = {
             const hora = removerSegundos(coordenadorInfo.quadroHorario[0].horaInicio);
             console.log(hora)
 
-            const speakOutput = `O coordenador ${coordenadorInfo.nome} está disponível na unidade ${coordenadorInfo.quadroHorario[0].descricao} , apartir ${hora}.`;
+            const diaProximo = diaMaisProximo(coordenadorInfo);
+            console.log(diaProximo);
+            
+            const diaExtenso = abreviacaoParaDiaExtenso(diaProximo);
+            console.log(diaExtenso);
+            
+            const speakOutput = `O coordenador ${coordenadorInfo.nome} está disponível na unidade ${coordenadorInfo.quadroHorario[0].descricao} , no dia ${diaExtenso}apartir ${hora}.`;
             
             parTelaHome.ExibirTelaHome(handlerInput);
 
